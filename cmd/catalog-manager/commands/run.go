ea756cf37980224af350d5837af257546d729bd8
Carmendelope <carmen@napptive.com>
2021-05-26 17:54:03 +0200

    adds analytics interceptor

--- cmd/catalog-manager/commands/run.go
+++ cmd/catalog-manager/commands/run.go
@@ -19,6 +19,7 @@ package commands
 import (
 	catalog_manager "github.com/napptive/catalog-manager/internal/app/catalog-manager"
 	"github.com/spf13/cobra"
+	"time"
 )
 
 var runCmdLongHelp = "Launch the catalog-manager service"
@@ -48,7 +49,15 @@ func init() {
 	runCmd.Flags().BoolVar(&cfg.JWTConfig.AuthEnabled, "authEnabled", false, "Enable Authentication")
 	runCmd.Flags().StringVar(&cfg.JWTConfig.Header, "authHeader", "authorization", "Authorization header name")
 	runCmd.Flags().StringVar(&cfg.JWTConfig.Secret, "authSecret", "secret", "Authorization secret to validate JWT signatures")
-	runCmd.Flags().BoolVar(&cfg.Enabled, "teamPrivileges", false, "Enable Team Privileges")
+	runCmd.Flags().BoolVar(&cfg.TeamConfig.Enabled, "teamPrivileges", false, "Enable Team Privileges")
 	runCmd.Flags().StringSliceVar(&cfg.PrivilegedUsers, "teamUsers", nil, "Privileged Users")
 	runCmd.Flags().StringSliceVar(&cfg.TeamNamespaces, "teamRepositories", nil, "Team Repositories")
+
+	runCmd.Flags().BoolVar(&cfg.BQConfig.Enabled, "analyticsEnabled", false, "Analytics enabled")
+	runCmd.Flags().StringVar(&cfg.BQConfig.Config.ProjectID, "analyticsProjectID", "playground", "GKE project for analytics")
+	runCmd.Flags().StringVar(&cfg.BQConfig.Config.CredentialsPath, "analyticsCredentialsPath", "/analytics/credentials.json", "credentials path for analytics")
+	runCmd.Flags().StringVar(&cfg.BQConfig.Config.Schema, "analyticsSchema", "analytics", "analytics schema")
+	runCmd.Flags().StringVar(&cfg.BQConfig.Config.Table, "analyticsTable", "operation", "analytics table")
+	runCmd.Flags().DurationVar(&cfg.BQConfig.Config.SendingTime, "analyticsLoop", 5 * time.Second, "time to send the data to analytics provider")
+
 }
--- go.mod
+++ go.mod
@@ -7,22 +7,23 @@ require (
 	github.com/elastic/go-elasticsearch/v8 v8.0.0-20210429082631-bdab472a9cc7
 	github.com/envoyproxy/protoc-gen-validate v0.6.1 // indirect
 	github.com/golang/mock v1.5.0
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.4.0
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+	github.com/napptive/analytics v0.0.0-20210526095254-9455f97b9c68
 	github.com/napptive/grpc-catalog-common-go v0.2.0
 	github.com/napptive/grpc-catalog-go v0.9.0
 	github.com/napptive/mockup-generator v1.0.0
 	github.com/napptive/nerrors v1.0.0
 	github.com/napptive/njwt v1.0.1-0.20210419162054-4691ac61a8b9
 	github.com/napptive/rdbms v1.0.0-rc5
-	github.com/onsi/ginkgo v1.16.1
-	github.com/onsi/gomega v1.11.0
+	github.com/onsi/ginkgo v1.16.2
+	github.com/onsi/gomega v1.12.0
 	github.com/rs/zerolog v1.21.0
 	github.com/spf13/cobra v1.1.3
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
-	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781
-	google.golang.org/genproto v0.0.0-20210429181445-86c259c2b4ab // indirect
+	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420
 	google.golang.org/grpc v1.37.0
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	sigs.k8s.io/yaml v1.2.0
