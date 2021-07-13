1ababc0b6493d8cc026405b0bcb2a1436ca64e38
Daniel Higuero <daniel@napptive.com>
2021-07-13 15:24:12 +0200

    PG-398 Enforce new JWT content

--- internal/pkg/server/catalog-manager/mocks.go
+++ internal/pkg/server/catalog-manager/mocks.go
@@ -17,7 +17,8 @@ package catalog_manager
 
 //go:generate  mockgen -destination metadata_provider_mock.go -package=catalog_manager github.com/napptive/catalog-manager/internal/pkg/provider/metadata MetadataProvider
 //go:generate  mockgen -destination storage_mock.go -package=catalog_manager github.com/napptive/catalog-manager/internal/pkg/storage StorageManager
-////go:generate  mockgen -destination manager_mock.go -self_package  github.com/napptive/catalog-manager/internal/server/catalog-manager -package=catalog_manager . Manager
+//go:generate  mockgen -destination catalog_add_server_mock.go -package=catalog_manager github.com/napptive/grpc-catalog-go Catalog_AddServer
+//go:generate  mockgen -destination manager_mock.go -self_package  github.com/napptive/catalog-manager/internal/server/catalog-manager -package=catalog_manager . Manager
 
 //Mock is a place holder to unify all mock generators.
 func Mock() {}
--- internal/pkg/server/catalog-manager/storage_mock.go
+++ internal/pkg/server/catalog-manager/storage_mock.go
@@ -5,36 +5,35 @@
 package catalog_manager
 
 import (
-	reflect "reflect"
-
 	gomock "github.com/golang/mock/gomock"
 	entities "github.com/napptive/catalog-manager/internal/pkg/entities"
+	reflect "reflect"
 )
 
-// MockStorageManager is a mock of StorageManager interface.
+// MockStorageManager is a mock of StorageManager interface
 type MockStorageManager struct {
 	ctrl     *gomock.Controller
 	recorder *MockStorageManagerMockRecorder
 }
 
-// MockStorageManagerMockRecorder is the mock recorder for MockStorageManager.
+// MockStorageManagerMockRecorder is the mock recorder for MockStorageManager
 type MockStorageManagerMockRecorder struct {
 	mock *MockStorageManager
 }
 
-// NewMockStorageManager creates a new mock instance.
+// NewMockStorageManager creates a new mock instance
 func NewMockStorageManager(ctrl *gomock.Controller) *MockStorageManager {
 	mock := &MockStorageManager{ctrl: ctrl}
 	mock.recorder = &MockStorageManagerMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use.
+// EXPECT returns an object that allows the caller to indicate expected use
 func (m *MockStorageManager) EXPECT() *MockStorageManagerMockRecorder {
 	return m.recorder
 }
 
-// ApplicationExists mocks base method.
+// ApplicationExists mocks base method
 func (m *MockStorageManager) ApplicationExists(arg0, arg1, arg2 string) (bool, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ApplicationExists", arg0, arg1, arg2)
@@ -43,13 +42,13 @@ func (m *MockStorageManager) ApplicationExists(arg0, arg1, arg2 string) (bool, e
 	return ret0, ret1
 }
 
-// ApplicationExists indicates an expected call of ApplicationExists.
+// ApplicationExists indicates an expected call of ApplicationExists
 func (mr *MockStorageManagerMockRecorder) ApplicationExists(arg0, arg1, arg2 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ApplicationExists", reflect.TypeOf((*MockStorageManager)(nil).ApplicationExists), arg0, arg1, arg2)
 }
 
-// CreateRepository mocks base method.
+// CreateRepository mocks base method
 func (m *MockStorageManager) CreateRepository(arg0 string) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CreateRepository", arg0)
@@ -57,13 +56,13 @@ func (m *MockStorageManager) CreateRepository(arg0 string) error {
 	return ret0
 }
 
-// CreateRepository indicates an expected call of CreateRepository.
+// CreateRepository indicates an expected call of CreateRepository
 func (mr *MockStorageManagerMockRecorder) CreateRepository(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateRepository", reflect.TypeOf((*MockStorageManager)(nil).CreateRepository), arg0)
 }
 
-// GetApplication mocks base method.
+// GetApplication mocks base method
 func (m *MockStorageManager) GetApplication(arg0, arg1, arg2 string, arg3 bool) ([]*entities.FileInfo, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetApplication", arg0, arg1, arg2, arg3)
@@ -72,13 +71,13 @@ func (m *MockStorageManager) GetApplication(arg0, arg1, arg2 string, arg3 bool) 
 	return ret0, ret1
 }
 
-// GetApplication indicates an expected call of GetApplication.
+// GetApplication indicates an expected call of GetApplication
 func (mr *MockStorageManagerMockRecorder) GetApplication(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetApplication", reflect.TypeOf((*MockStorageManager)(nil).GetApplication), arg0, arg1, arg2, arg3)
 }
 
-// RemoveApplication mocks base method.
+// RemoveApplication mocks base method
 func (m *MockStorageManager) RemoveApplication(arg0, arg1, arg2 string) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "RemoveApplication", arg0, arg1, arg2)
@@ -86,13 +85,13 @@ func (m *MockStorageManager) RemoveApplication(arg0, arg1, arg2 string) error {
 	return ret0
 }
 
-// RemoveApplication indicates an expected call of RemoveApplication.
+// RemoveApplication indicates an expected call of RemoveApplication
 func (mr *MockStorageManagerMockRecorder) RemoveApplication(arg0, arg1, arg2 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RemoveApplication", reflect.TypeOf((*MockStorageManager)(nil).RemoveApplication), arg0, arg1, arg2)
 }
 
-// RemoveRepository mocks base method.
+// RemoveRepository mocks base method
 func (m *MockStorageManager) RemoveRepository(arg0 string) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "RemoveRepository", arg0)
@@ -100,13 +99,13 @@ func (m *MockStorageManager) RemoveRepository(arg0 string) error {
 	return ret0
 }
 
-// RemoveRepository indicates an expected call of RemoveRepository.
+// RemoveRepository indicates an expected call of RemoveRepository
 func (mr *MockStorageManagerMockRecorder) RemoveRepository(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RemoveRepository", reflect.TypeOf((*MockStorageManager)(nil).RemoveRepository), arg0)
 }
 
-// RepositoryExists mocks base method.
+// RepositoryExists mocks base method
 func (m *MockStorageManager) RepositoryExists(arg0 string) (bool, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "RepositoryExists", arg0)
@@ -115,13 +114,13 @@ func (m *MockStorageManager) RepositoryExists(arg0 string) (bool, error) {
 	return ret0, ret1
 }
 
-// RepositoryExists indicates an expected call of RepositoryExists.
+// RepositoryExists indicates an expected call of RepositoryExists
 func (mr *MockStorageManagerMockRecorder) RepositoryExists(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RepositoryExists", reflect.TypeOf((*MockStorageManager)(nil).RepositoryExists), arg0)
 }
 
-// StoreApplication mocks base method.
+// StoreApplication mocks base method
 func (m *MockStorageManager) StoreApplication(arg0, arg1, arg2 string, arg3 []*entities.FileInfo) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "StoreApplication", arg0, arg1, arg2, arg3)
@@ -129,7 +128,7 @@ func (m *MockStorageManager) StoreApplication(arg0, arg1, arg2 string, arg3 []*e
 	return ret0
 }
 
-// StoreApplication indicates an expected call of StoreApplication.
+// StoreApplication indicates an expected call of StoreApplication
 func (mr *MockStorageManagerMockRecorder) StoreApplication(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "StoreApplication", reflect.TypeOf((*MockStorageManager)(nil).StoreApplication), arg0, arg1, arg2, arg3)
